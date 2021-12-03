c6ff3fe86cb21fc67e19b8837295e4d85588f304
Astrospence <illustrator.wood@gmail.com>
2021-12-03 11:29:01 -0700

    test suite successful

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,38 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
-
-test("shows success message on submit with form details", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm />);
+});
+    
+test("shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+    const firstNameField = screen.getByLabelText("First Name:");
+    const lastNameField = screen.getByLabelText("Last Name:");
+    const addressField = screen.getByLabelText("Address:");
+    const cityField = screen.getByLabelText("City:");
+    const stateField = screen.getByLabelText("State:");
+    const zipField = screen.getByLabelText("Zip:");
+    const submit = screen.queryByRole("button");
+    await waitFor(() => {
+        const successMessage = screen.queryByTestId("successMessage");
+        userEvent.type(firstNameField, "SomeUser");
+        userEvent.type(lastNameField, "SomeLastName");
+        userEvent.type(addressField, "Test Avenue");
+        userEvent.type(cityField, "SomeCity");
+        userEvent.type(stateField, "SomeState");
+        userEvent.type(zipField, "77777");
+        userEvent.click(submit);
+        expect(successMessage).toBeTruthy();
+        expect(successMessage).toHaveTextContent("You have ordered some plants!");
+        expect(successMessage).toHaveTextContent("Your new green friends will be shipped to:");
+        expect(successMessage).toHaveTextContent("SomeUser SomeLastName");
+        expect(successMessage).toHaveTextContent("Test Avenue");
+        expect(successMessage).toHaveTextContent("SomeCity, SomeState 77777");
+    });    
+});
